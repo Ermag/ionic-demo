d86903c67aa86f9c0a550b4e6eee905e358b33ba
Ermag <ermag.bg@gmail.com>
2023-03-04 15:14:45 +0200

    Bootstrap trough Ionic CLI (Vue).

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'mayven.task',
+  appName: 'mayven-task',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
